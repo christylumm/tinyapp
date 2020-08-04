1663b10cbac5c612f79f1cbee2ace79356bdc762
Christy Lum <christy.lum121@gmail.com>
2020-08-04 05:31:32 +0000

    Added a GET route to show form, added a POST route to receive the form submission, generate random short URL

--- express_server.js
+++ express_server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +25,11 @@ app.get("/hello", (req, res) => {
 });
 
 //Add a route for /urls and use res.render() to pass the URL data to our template
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -35,6 +43,25 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let result = '';
+
+  //generate string based off these alphanumeric characters
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+console.log(generateRandomString());
